dcea8466dca054c0513339b4ffce70ab68dd7a4c
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-07-30 17:28:40 +0700

    Initial Commit

---
+++ FoodCal/FoodCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCalApp.swift
+//  FoodCal
+//
+//  Created by Tuấngg Anhhh on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodCalTests/FoodCalTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCalTests.swift
+//  FoodCalTests
+//
+//  Created by Tuấngg Anhhh on 30/7/25.
+//
+
+import Testing
+@testable import FoodCal
+
+struct FoodCalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
